05ea7112d3642224a8cdfdd3ab213e47cea1a0cb
knng <kungbill79@gmail.com>
2024-04-25 16:50:35 +0800

    add post animation, set the max remember QA

--- chatbot_frontend/src/components/ChatBox.vue
+++ chatbot_frontend/src/components/ChatBox.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="chatbox-container">
     <div class="container">
-      <h1>Azure Open AI bot</h1>
-      <div class="messageBox mt-8">
+      <h1>AI Chat Bot</h1>
+      <div ref="messageBox" class="messageBox mt-8">
         <template v-for="(message, index) in messages" :key="index">
-          <div v-if="message.role != 'system'">
-            <div :class="message.role == 'user' ? 'messageFromUser' : 'messageFromChatGpt'">
-              <div :class="message.role == 'user' ? 'userMessageWrapper' : 'chatGptMessageWrapper'">
-                <div :class="message.role == 'user' ? 'userMessageContent' : 'chatGptMessageContent'">{{ message.content
-                  }}
-                </div>
+          <div v-if="message.content.trim().length !== 0"
+            :class="message.role == 'user' ? 'messageFromUser' : 'messageFromChatGpt'">
+            <div :class="message.role == 'user' ? 'userMessageWrapper' : 'chatGptMessageWrapper'">
+              <div :class="message.role == 'user' ? 'userMessageContent' : 'chatGptMessageContent'">
+                {{ message.content }}
               </div>
             </div>
+            <img v-if="message.role == 'assistant'" :src='require(`../assets/bot.png`)' width="40" height="40">
           </div>
-
         </template>
       </div>
       <div class="inputContainer">
-        <input v-model="currentMessage" type="text" class="messageInput" placeholder="請輸入問題" @keyup.enter="sendMessage(currentMessage)" />
-        <button @click="sendMessage(currentMessage)" class="askButton">
-          Ask
+        <input v-model="currentMessage" type="text" class="messageInput" :disabled="processing"
+          placeholder="Please enter your question" @keyup.enter="sendMessage(currentMessage)" />
+        <button :disabled="processing" @click="sendMessage(currentMessage)" class="askButton">
+          <i v-if="!processing" class="pi pi-send" style="font-size: 1rem"></i>
+          <i v-if="processing" class="pi pi-spin pi-spinner" style="font-size: 1rem"></i>
+
         </button>
       </div>
     </div>
@@ -29,6 +31,7 @@
 
 <script>
 import axios from 'axios';
+import 'primeicons/primeicons.css'
 
 
 export default {
@@ -36,30 +39,61 @@ export default {
   data() {
     return {
       currentMessage: '',
-      messages: [{
+      system_prompt: {
         "role": "system",
-        "content": "You are a helpful assistant. You will talk like a pirate."
-      }],
+        "content": "you are a helpful assiatant."
+      },
+      processing: false,
+      messages: [],
     };
   },
   methods: {
+    sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    },
     async sendMessage(currentMessage) {
+      if (currentMessage.trim().length === 0) {
+        return
+      }
+      this.processing = true
+      //update messages
       this.messages.push({
         role: 'user',
         content: currentMessage,
       });
+
+      var submitted_messages = []
+      submitted_messages.push(this.system_prompt) //Fristly, insert the system prompt 
+      //only remember recently 7 content
+      let remember_num = 7
+      if (this.messages.length > remember_num) {
+        for (let i = 0; i < remember_num; ++i) {
+          submitted_messages[i + 1] = this.messages[this.messages.length - remember_num + i];
+        }
+      } else {
+        for (let i = 0; i < this.messages.length; ++i) {
+          submitted_messages[i + 1] = this.messages[i]
+        }
+      }
       await axios
-        .post('https://localhost:7139/api/chat', this.messages
+        .post('https://localhost:7152/api/chat', submitted_messages
         )
         .then((response) => {
           this.messages.push({
             role: 'assistant',
             content: response.data, // Access the 'data' property of the response object
           });
-          this.currentMessage = ''
+        }).catch(function (error) {
+          console.log(error.toJSON());
         });
-
-    },
+      this.currentMessage = ''
+      this.processing = false
+    }
+  },
+  updated: function () {
+    //after render all message, scroll down 
+    const messageBox = this.$refs.messageBox;
+    messageBox.scrollTop = messageBox.scrollHeight;
   },
 };
 </script>
@@ -114,7 +148,7 @@ h1 {
 
 
 .messageBox {
-  max-height: 400px;
+  max-height: 450px;
   overflow-y: auto;
   padding: 0 16px;
   border-top: 1px solid #f0f0f0;
@@ -125,7 +159,7 @@ h1 {
 .messageFromUser,
 .messageFromChatGpt {
   display: flex;
-  margin-bottom: 8px;
+  margin-bottom: 2px;
 }
 
 .userMessageWrapper,
@@ -144,7 +178,7 @@ h1 {
 
 .userMessageContent,
 .chatGptMessageContent {
-  max-width: 60%;
+  max-width: 85%;
   padding: 8px 12px;
   border-radius: 18px;
   margin-bottom: 2px;
@@ -230,7 +264,7 @@ h1 {
 
 
 .messageBox {
-  padding: 16px;
+  padding: 20px;
   flex-grow: 1;
   overflow-y: auto;
   display: flex;
